e2b4020283f4eb76a613bff7213ed5d58155ef16
RaissaMara <raissa.mara122@gmail.com>
2026-09-10 11:42:16 -0300

    Initial

---
+++ Praticando_C#_Alura/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Praticando_C__Alura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Exemplo alura (Ano 1997)
+            int anoNascimento = 2002;
+            int anoVigente = 2026;
+
+            int idade = anoVigente - anoNascimento;
+            Console.WriteLine("Idade: " + idade);
+           
+            //Exemplo alura
+            float valorRecebido = 500.00f;
+            bool doacaoAnonima = true;
+            char tipoConta = 'P';
+
+            Console.WriteLine("Valor recibido: R$" + valorRecebido);
+            Console.WriteLine("Doação Anônima: " + doacaoAnonima);
+            Console.WriteLine("Tipo de Conta:" + tipoConta);
+
+            //Meu exemplo de treino
+            double milha = 1.60934;
+            Console.Write("Digite a quantidade de milhas desejadas e converteremos para quilometragem:");
+            double quantidade = double.Parse(Console.ReadLine()!);
+            double equivalem = quantidade * milha;
+            Console.WriteLine($"{quantidade} milhas equivalem a ${equivalem} quilomêtros");
+
+            //Exemplo alura
+            double milhas = 10.0;
+            double quilometros = milhas * 1.60934;
+            Console.WriteLine(milhas + " milhas equivalem a " + quilometros + " quilomêtros.");
+
+            int totalMinutos = 250;
+            int horas = totalMinutos / 60;
+            int minutosRestante = totalMinutos % 60;
+            Console.Write($"Tempo: {horas} horas e {minutosRestante} minutos");
+
+            //Exemplo alura
+            int minutos = 250;
+            int hora = minutos / 60;
+            int restantesMinutos = minutos % 60;
+
+            Console.WriteLine("Tempo: " + hora + " horas e " + restantesMinutos + " minutos");
+
+        }
+    }
+}
